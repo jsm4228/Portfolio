001a5e24822f8da569eed3f7477da58ca97f1d46
Josh Mathew <jsmathew41@gmail.com>
2023-07-14 14:17:56 -0500

    updated projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -167,8 +167,9 @@ const experiences = [
 const projects = [
   {
     id: "project-1",
-    name: "KomiKult",
-    description: "A comic characters list app that displays Marvel characters.",
+    name: "Chat App with ChatGPT",
+    description:
+      "A real-time chat applications with user authentication, OpenAI ChatGPT integration, friend request, and real-time messages powered by socket.io",
     tags: [
       {
         name: "react",
@@ -184,14 +185,14 @@ const projects = [
       },
     ],
     image: komikult,
-    repo: "https://github.com/shaqdeff/KomiKult",
-    demo: "https://shaqdeff.github.io/KomiKult/",
+    repo: "https://github.com/kevinleet/helloWorld_frontend",
+    demo: "https://helloworldfrontend-production.up.railway.app/",
   },
   {
     id: "project-2",
-    name: "Leaderboard",
+    name: "Cocktail Recipes",
     description:
-      "A leaderboard list app that displays scores submitted by different players.",
+      "A recipe application utilzing a 3rd-party API to beatifully and easily display recipes for thousands of cocktails with React.js",
     tags: [
       {
         name: "react",
@@ -207,76 +208,76 @@ const projects = [
       },
     ],
     image: leaderboard,
-    repo: "https://github.com/shaqdeff/Leaderboard",
-    demo: "https://shaqdeff.github.io/Leaderboard/",
+    repo: "https://github.com/jsm4228/u3_lab_react_axios_foodE/blob/main/README.md",
+    demo: "https://cocktail-recipes-123.netlify.app/",
   },
-  {
-    id: "project-3",
-    name: "Math Magicians",
-    description: "This is a single-page calculator app built with React",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: math,
-    repo: "https://github.com/shaqdeff/Math-Magicians",
-    demo: "https://inspiring-medovik-37d3b3.netlify.app/",
-  },
-  {
-    id: "project-4",
-    name: "Movie Metro",
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: movie,
-    repo: "https://github.com/shaqdeff/Movie-Metro",
-    demo: "https://movie-metro.netlify.app/",
-  },
-  {
-    id: "project-5",
-    name: "Nyeusi Fest Site",
-    description:
-      "This is a demo concert website for a music festival called Nyeusi.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: nyeusi,
-    repo: "https://github.com/shaqdeff/Nyeusi-Fest-Site",
-    demo: "https://shaqdeff.github.io/Nyeusi-Fest-Site/",
-  },
+  // {
+  //   id: "project-3",
+  //   name: "Math Magicians",
+  //   description: "This is a single-page calculator app built with React",
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: math,
+  //   repo: "https://github.com/shaqdeff/Math-Magicians",
+  //   demo: "https://inspiring-medovik-37d3b3.netlify.app/",
+  // },
+  // {
+  //   id: "project-4",
+  //   name: "Movie Metro",
+  //   description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: movie,
+  //   repo: "https://github.com/shaqdeff/Movie-Metro",
+  //   demo: "https://movie-metro.netlify.app/",
+  // },
+  // {
+  //   id: "project-5",
+  //   name: "Nyeusi Fest Site",
+  //   description:
+  //     "This is a demo concert website for a music festival called Nyeusi.",
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: nyeusi,
+  //   repo: "https://github.com/shaqdeff/Nyeusi-Fest-Site",
+  //   demo: "https://shaqdeff.github.io/Nyeusi-Fest-Site/",
+  // },
 ];
 
 export { services, technologies, experiences, projects };
